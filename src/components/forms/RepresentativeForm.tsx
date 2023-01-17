a3f7b30fd55efb6140092245e3d27130eed8beb5
Foltrex <fpm.chernenkAV@bsu.by>
2023-01-17 23:45:38 +0300

    Add reprentative form data fetching and update some fields

--- src/components/forms/RepresentativeForm.tsx
+++ src/components/forms/RepresentativeForm.tsx
@@ -1,36 +1,69 @@
-import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, Grid, Divider } from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, Grid, Divider, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Representative } from '../../entities/representative';
 
-interface RepresentativeFormProps {
-    open: boolean
-    handleClose: () => void
+interface IRepresentativeFormProps {
+    open: boolean;
+    handleClose: () => void;
+    representative?: Representative
 }
 
-const RepresentativeForm = ({open, handleClose} : RepresentativeFormProps) => {
+const RepresentativeForm = ({open, handleClose, representative } : IRepresentativeFormProps) => {
+    const [representativeObj, setRepresentative] = useState({});
+
+    useEffect(() => {
+        if (representative) {
+            const { organization, facility } = representative;
+        }
+
+        setRepresentative({...representative})
+    }, [])
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setRepresentative({...representativeObj, [name]: value})
+    }
+
+    const handleSelectChange = (e: SelectChangeEvent<string>) => {
+        console.log(e.target);
+        
+        const { value } = e.target;
+    }
+
+
+    const handleSaveButtonClick = () => {
+        console.log(representativeObj);
+    }
+
+
     return (
       <Dialog open={open} onClose={handleClose} maxWidth='xs'>
-        <DialogTitle>Create Representative</DialogTitle>
+        <DialogTitle>{representative ? 'Edit' : 'Create'} Representative</DialogTitle>
         <Divider />
         <DialogContent>
             <Grid container rowSpacing='3'>
                 <Grid item xs={12}>
                     <TextField
                         autoFocus
-                        id="email"
-                        label="Email"
+                        id="firstname"
+                        name='firstname'
+                        label="Firstname"
                         type="name"
                         fullWidth
                         required
+                        defaultValue={representative?.firstname}
                         variant="standard"
                     />
                 </Grid>
                 <Grid item xs={12}>
                     <TextField 
-                        id='role'
-                        label='Role'
+                        id='lastname'
+                        name='lastname'
+                        label='Lastname'
                         fullWidth
+                        defaultValue={representative?.lastname}
                         variant='standard'
                         type='name'
                         required
@@ -38,29 +71,56 @@ const RepresentativeForm = ({open, handleClose} : RepresentativeFormProps) => {
                 </Grid>
                 <Grid item xs={12}>
                     <TextField 
-                        id='facility'
-                        label='Facility'
+                        id='organization'
+                        name='organization'
+                        label='Organization'
+                        disabled
                         fullWidth
                         variant='standard'
                         type='name'
+                        defaultValue={representative?.organization?.name }
                         required
                     />
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField 
-                        id='action'
-                        label='Action'
-                        fullWidth
-                        variant='standard'
-                        type='name'
-                        required
-                    />
+                    <FormControl fullWidth variant='standard'>
+                        <InputLabel id="facility-label">Facility</InputLabel>
+                        <Select
+                            labelId="facility-label"
+                            id='facility'
+                            name='facility'
+                            label='Facility'
+                            required
+                            onChange={handleSelectChange}
+                        >
+                            <MenuItem value={10}>First</MenuItem>
+                            <MenuItem value={20}>Second</MenuItem>
+                            <MenuItem value={30}>Third</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                    <FormControl fullWidth variant='standard'>
+                        <InputLabel id="organization-role-label">Organization Role</InputLabel>
+                        <Select
+                            labelId="organization-role-label"
+                            id="organizationRole"
+                            name='organizationRole'
+                            // value={age}
+                            label="Organization Role"
+                            onChange={handleSelectChange}
+                        >
+                            <MenuItem value={10}>Admin</MenuItem>
+                            <MenuItem value={20}>NeAdmin</MenuItem>
+                            <MenuItem value={30}>Maybe admin</MenuItem>
+                        </Select>
+                    </FormControl>
                 </Grid>
             </Grid>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} variant='text'>Cancel</Button>
-          <Button onClick={handleClose} variant='contained'>Save</Button>
+          <Button onClick={handleSaveButtonClick} variant='contained'>Save</Button>
         </DialogActions>
       </Dialog>
     );
