5ac1978eccd4b601cd56c39d3946b3dc7364eee3
Foltrex <temach2002@gmail.com>
2023-01-19 14:31:56 +0300

    Add typization to organization api

--- src/api/OrganizationApi.ts
+++ src/api/OrganizationApi.ts
@@ -1,7 +1,8 @@
+import { Organization } from "../entities/organization";
 import { ART_SERVICE, axiosApi } from "../http/axios";
 
 export class OrganizationApi {
     static getAllOrganizations() {
-        return axiosApi.get(`${ART_SERVICE}/organizations`);
+        return axiosApi.get<Array<Organization>>(`${ART_SERVICE}/organizations`);
     }
 }
