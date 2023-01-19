c44626beed7f9ce693c0c2d4c1019a95508be7ca
Foltrex <temach2002@gmail.com>
2023-01-19 14:36:42 +0300

    Delete store context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,12 @@
 import type { AppProps } from 'next/app';
 import Layout from '../components/layout/Layout';
 import '../styles/globals.css';
-import { RootStoreProvider } from './rootStoreAdapter';
 
 export default function App({Component, pageProps}: AppProps) {
 
     return (
-        <RootStoreProvider>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </RootStoreProvider>
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
     );
 }
